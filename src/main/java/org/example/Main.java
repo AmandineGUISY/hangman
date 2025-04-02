170d15bbc19101f5811881a7cb378b21835d2092
AmandineGUISY <aguisy@yahoo.fr>
2025-04-02 18:55:38 +0200

    Display of the hangman, testing the display on the hangman function in the Hangman class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String path = "src/main/resources/words.txt";
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String found = "";
+        int level = 1;
 
-        List<String> words = getWords.words();
+        Scanner scanner = new Scanner(System.in);
+        List<String> words = getWords.words(path);
+        Hangman hangman = new Hangman();
+        hangman.hangman(0);
+        hangman.hangman(1);
+        hangman.hangman(2);
+        hangman.hangman(3);
+        hangman.hangman(4);
+        hangman.hangman(5);
+        hangman.hangman(6);
 
-        System.out.println("Nombre de mots chargés: " + words.size());
+        while(level < words.size() + 1) {
+            level++;
+        }
+
+        System.out.println("Nombre de mots chargés: " + words.get(1));
         System.out.println("Liste des mots:" + words);
     }
 }
