a920310992367b244f7734fcbe62c5c2dbc05948
AmandineGUISY <aguisy@yahoo.fr>
2025-04-02 17:25:13 +0200

    Using the getWords class, and it's working fine

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String path = "src/main/resources/words.txt";
+
+        List<String> words = getWords.words();
+
+        System.out.println("Nombre de mots chargés: " + words.size());
+        System.out.println("Liste des mots:" + words);
     }
 }
