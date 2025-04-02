3d6f966c79dabfb4ef2620431f12ccad2e51d32e
AmandineGUISY <aguisy@yahoo.fr>
2025-04-02 18:54:52 +0200

    Init of the hangman class with a function hangman who can display a hanged man if function of the errors

--- src/main/java/org/example/Hangman.java
+++ src/main/java/org/example/Hangman.java
@@ -1,4 +1,79 @@
 package org.example;
 
+// word : is the word we are trying to guess
+// letter : is the letter he didn't find or tried yet
+// guess : is the letter he is trying to guess
+
 public class Hangman {
+    public void hangman (int error) {
+        switch (error) {
+            case 0:
+                System.out.println(
+                        "\n\n\n\n\n" +
+                        "=========\n");
+                break;
+            case 1:
+                System.out.println(
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========\n");
+                break;
+            case 2:
+                System.out.println(
+                        "  +---+\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========\n");
+                break;
+            case 3:
+                System.out.println(
+                        "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========\n");
+                break;
+            case 4:
+                System.out.println(
+                        "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        "  |   |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========\n");
+                break;
+            case 5:
+                System.out.println(
+                        "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        " /|\\ |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "=========\n");
+                break;
+            case 6:
+                System.out.println(
+                        "  +---+\n" +
+                        "  |   |\n" +
+                        "  O   |\n" +
+                        " /|\\  |\n" +
+                        " / \\  |\n" +
+                        "      |\n" +
+                        "=========\n" +
+                        "Perdu ! Le pendu est complet...");
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
 }
