13dd54e6457812c19398175c012de9b54d491b0a
AmandineGUISY <aguisy@yahoo.fr>
2025-04-03 10:42:14 +0200

    Making the Verify class with his function verify in order to controle scanner enter

--- src/main/java/org/example/Verify.java
+++ src/main/java/org/example/Verify.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Verify {
+    public static int verify(String letter, String tried, String alphabet) {
+        if(letter.length() != 1) {
+            System.out.println("Invalid letter");
+            return 1;
+        }
+        if(!alphabet.contains(letter)) {
+            System.out.println("Your letter is not in the alphabet");
+            return 1;
+        }
+        if(tried.contains(letter)) {
+            System.out.println("You already tried this letter");
+            return 1;
+        }
+        return 0;
+    }
 }
