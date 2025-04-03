45f07741b10f7a19b8ac46ae225e41a96803d00c
AmandineGUISY <aguisy@yahoo.fr>
2025-04-03 11:21:43 +0200

    Using the update of the errors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,14 +17,22 @@ public class Main {
         Hangman hangman = new Hangman();
 
         while(error < 6) {
+
             hangman.hangman(error);
             System.out.print("Choisi une lettre : ");
             letter = scanner.nextLine().trim().toUpperCase();
+
             while (Verify.verify(letter, tried, alphabet) == 1) {
                 System.out.print("Lettre invalide. Réessaie : ");
                 letter = scanner.nextLine().trim().toUpperCase();
             }
+
+            error = Verify.errors(letter, words.get(level), error);
+            tried = tried + letter;
+            System.out.println(words.get(level));
+
         }
+        hangman.hangman(error);
 
     }
 }
