28ddd920e238c9c415e4d749b9716f063eae8d72
AmandineGUISY <aguisy@yahoo.fr>
2025-04-03 13:50:27 +0200

    Addition of a function who display the word in function of the letter found + resolving a bug with the display of the available letter, they displayed only the first tried one

--- src/main/java/org/example/Hangman.java
+++ src/main/java/org/example/Hangman.java
@@ -79,17 +79,33 @@ public class Hangman {
 
     public void availableLetters (String tried, String alphabet) {
         for(int i = 0; i < alphabet.length(); i++) {
-            if(tried.indexOf(alphabet.charAt(i)) == 0) {
+            if(tried.indexOf(alphabet.charAt(i)) >= 0) {
                 System.out.print(" ");
             }
             else {
                 System.out.print(alphabet.charAt(i) + " ");
             }
+
             if (i == 8 || i == 17 || i == 25) {System.out.println();}
         }
 
         if (!tried.isEmpty()) {
             System.out.println("\nLettres essayées: " + tried);
         }
+        System.out.println();
+    }
+
+    public void hangmanWord (String word, String tried) {
+        word = word.toUpperCase();
+
+        for(int i = 0; i < word.length(); i++) {
+            if(tried.indexOf(word.charAt(i)) == -1) {
+                System.out.print("_ ");
+            }
+            else {
+                System.out.print(word.charAt(i) + " ");
+            }
+        }
+        System.out.println("\n");
     }
 }
